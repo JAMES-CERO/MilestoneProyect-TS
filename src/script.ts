34304cb9617e464c5870d985498800f4a4e23d22
James Aules <james_aules@icloud.com>
2023-06-08 21:35:02 -0400

    convert functions

--- src/script.ts
+++ src/script.ts
@@ -29,11 +29,13 @@ function findpiece(pieceId: number):number {
     //https://www.w3schools.com/jsref/dom_obj_var.asp
     //https://www.w3schools.com/jsref/met_document_queryselector.asp
 
-const cells = document.querySelectorAll("td");
-let redPieces = document.querySelectorAll("p");
-let blackPieces = document.querySelectorAll("span")
-const redTurnText = document.querySelectorAll(".redTurn");
-const blackTurnText = document.querySelectorAll(".blackTurn");
+const cells: NodeListOf<HTMLTableCellElement> = document.querySelectorAll("td");
+let redPieces: NodeListOf<HTMLParagraphElement> = document.querySelectorAll("p");
+let blackPieces: NodeListOf<HTMLSpanElement> = document.querySelectorAll("span")
+const redTurnText: NodeListOf<HTMLElement> = document.querySelectorAll(".redTurn");
+const blackTurnText: NodeListOf<HTMLElement> = document.querySelectorAll(".blackTurn");
+
+//By providing type annotations, you can leverage the benefits of TypeScript's static typing and catch potential type-related errors during development.
 
 //player properties
 
@@ -70,14 +72,14 @@ const blackTurnText = document.querySelectorAll(".blackTurn");
   }
  
 // Every time that the page loads is gonna show the message StartGame 
-function startgame(){
+function startgame():void {
     alert("Hey, welcome to checkers!. This game is my new Model Project, i hope you can enjoy this classic. Note: You can review the whole experience & how the game is building in my GitHub Account -- https://github.com/JAMES-CERO -- ")
 }
 startgame()
 
 
   // Add an event listener to each piece
-  function piecesEvent(){
+  function piecesEvent():void {
     if (turn) {
         for (let i = 0; i < redPieces.length; i++) {
             redPieces[i].addEventListener('click',  getPiecescount);
@@ -92,7 +94,7 @@ startgame()
   // function that removes Onclick on pieces, src https://forum.boardgamearena.com/viewtopic.php?t=17885&start=10
 // Element.removeAttribute() from https://developer.mozilla.org/en-US/docs/Web/API/Element/removeAttribute
 
-function removeOnClick(){
+function removeOnClick():void {
     for (let i = 0; i < cells.length; i++) {
         cells[i].removeAttribute("onclick");
     }
@@ -101,30 +103,34 @@ function removeOnClick(){
 
   //This is a funtion that hold the pieces count 
 
-  function getPiecescount(){
+  function getPiecescount():void {
+    let playerPieces: HTMLElement[];
+
     if (turn) {
-        playerPieces = redPieces; // line 14
+        playerPieces = Array.from(redPieces) ; // line 14
     } else {
-        playerPieces = blackPieces;// line 15
+        playerPieces = Array.from(blackPieces);// line 15
     }
     removeOnClick();
-    changeColorPiece();
+    changeColorPiece(playerPieces);
   }
 
 
 
 // we change the color of the piece that is choose & after thatr turn the piece  back to a normal piece 
 
-function changeColorPiece() {
+function changeColorPiece(playerPieces: HTMLElement[]): void {
     for (let i = 0; i < playerPieces.length; i++) {
-       playerPieces[i].style.border= "2px solid white";
+        playerPieces[i].style.border = "2px solid white";
     }
     resetPieceProperties();
-    getSelectedPiece();
+    const selectedPieceId = playerPieces[0].id; // Assuming the first element represents the selected piece
+    const indexPiece = findpiece(parseInt(selectedPieceId, 10));
+    getSelectedPiece(selectedPieceId, indexPiece);
 }
  // resset all the properties this is importante because this has to happen every cell click
  // we have all the possibles moves 
-function resetPieceProperties(){
+function resetPieceProperties():void{
     selectedPiece.pieceId = -1;
     selectedPiece.pieceId = -1;
     selectedPiece.isKing = false;
@@ -144,16 +150,17 @@ function resetPieceProperties(){
 
 // this function gets the id piece & the cell of the index board ; the event.target return a string , we need to make it a number 
 
-function getSelectedPiece(){
-    selectedPiece.pieceId = parseInt(event.target.id);
-    selectedPiece.indexPiece = findpiece(selectedPiece.pieceId);
+function getSelectedPiece(pieceId: string, indexPiece: number): void {
+    selectedPiece.pieceId = parseInt(pieceId, 10);
+    selectedPiece.indexPiece = indexPiece;
     ifPieceIsKing();
 }
 
 // element. classlist.contains  from https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
 
-function ifPieceIsKing(){
-    if(document.getElementById(selectedPiece.pieceId).classList.contains("king")){
+function ifPieceIsKing(): void {
+    const selectedPieceElement = document.getElementById(String(selectedPiece.pieceId));
+    if(selectedPieceElement && selectedPieceElement.classList.contains("king")){
       selectedPiece.isKing = true;
     }else{
       selectedPiece.isKing = false;
@@ -165,7 +172,7 @@ function ifPieceIsKing(){
 // possibles moves 
 // how to manipulate table cells from https://stackoverflow.com/questions/8508262/how-to-select-td-of-the-table-with-javascript
 
-function getEmptySpaces(){
+function getEmptySpaces():void {
     if(board[selectedPiece.indexPiece + 7] === null && 
        cells[selectedPiece.indexPiece + 7].classList.contains("noPieceHere") !== true) {
         selectedPiece.seventhSpace = true;
@@ -185,58 +192,86 @@ function getEmptySpaces(){
     getJumpSpaces();
 }
 
-function getJumpSpaces(){
-    
-    if(turn){
-        if(board[selectedPiece.indexPiece + 14] === null
-        && cells[selectedPiece.indexPiece + 14].classList.contains("noPieceHere") !== true 
-        && board[selectedPiece.indexPiece + 7] >= 12) {
-            selectedPiece.fourteenthSpace = true;
-        }
-        if(board[selectedPiece.indexPiece + 18] === null
-        && cells[selectedPiece.indexPiece + 18].classList.contains("noPieceHere") !== true 
-        && board[selectedPiece.indexPiece + 9] >= 12) {
-                selectedPiece.eightteenthSpace = true;
-        }
-        if(board[selectedPiece.indexPiece - 14] === null
-        && cells[selectedPiece.indexPiece - 14].classList.contains("noPieceHere") !== true 
-        && board[selectedPiece.indexPiece - 7] >= 12) {
-                selectedPiece.negativefourteenthSpace = true;
-            }
-        if(board[selectedPiece.indexPiece - 18] === null
-        && cells[selectedPiece.indexPiece - 18].classList.contains("noPieceHere") !== true 
-        && board[selectedPiece.indexPiece - 9] >= 12) {
-            selectedPiece.negativeEighteenthSpace = true;
-        }
-
-    }else{
-        if(board[selectedPiece.indexPiece + 14] === null
-         && cells[selectedPiece.indexPiece + 14].classList.contains("noPieceHere") !== true 
-         && board[selectedPiece.indexPiece + 7] < 12 && board[selectedPiece.indexPiece + 7] !== null ) {
-                selectedPiece.fourteenthSpace = true;
-            }
-        if(board[selectedPiece.indexPiece + 18] === null
-        && cells[selectedPiece.indexPiece + 18].classList.contains("noPieceHere") !== true 
-        && board[selectedPiece.indexPiece + 9] < 12 && board[selectedPiece.indexPiece + 9] !== null ) {
-                selectedPiece.eightteenthSpace = true;
-            }
-        if(board[selectedPiece.indexPiece - 14] === null
-        && cells[selectedPiece.indexPiece - 14].classList.contains("noPieceHere") !== true 
-        && board[selectedPiece.indexPiece - 7] < 12 && board[selectedPiece.indexPiece - 7] !== null ) {
-                selectedPiece.negativefourteenthSpace = true;
-            }
-        if(board[selectedPiece.indexPiece - 18] === null
-        && cells[selectedPiece.indexPiece - 18].classList.contains("noPieceHere") !== true 
-        && board[selectedPiece.indexPiece - 9] < 12 && board[selectedPiece.indexPiece - 9] !== null ) {
-                selectedPiece.negativeEighteenthSpace = true;
-            }
+function getJumpSpaces(): void {
+    if (turn) {
+      if (
+        board[selectedPiece.indexPiece + 14] === null &&
+        cells[selectedPiece.indexPiece + 14]?.classList.contains("noPieceHere") !== true &&
+        board[selectedPiece.indexPiece + 7] !== null &&
+        board[selectedPiece.indexPiece + 7] >= 12
+      ) {
+        selectedPiece.fourteenthSpace = true;
+      }
+      if (
+        board[selectedPiece.indexPiece + 18] === null &&
+        cells[selectedPiece.indexPiece + 18]?.classList.contains("noPieceHere") !== true &&
+        board[selectedPiece.indexPiece + 9] !== null &&
+        board[selectedPiece.indexPiece + 9] >= 12
+      ) {
+        selectedPiece.eighteenthSpace = true;
+      }
+      if (
+        board[selectedPiece.indexPiece - 14] === null &&
+        cells[selectedPiece.indexPiece - 14]?.classList.contains("noPieceHere") !== true &&
+        board[selectedPiece.indexPiece - 7] !== null &&
+        board[selectedPiece.indexPiece - 7] >= 12
+      ) {
+        selectedPiece.negativeFourteenthSpace = true;
+      }
+      if (
+        board[selectedPiece.indexPiece - 18] === null &&
+        cells[selectedPiece.indexPiece - 18]?.classList.contains("noPieceHere") !== true &&
+        board[selectedPiece.indexPiece - 9] !== null &&
+        board[selectedPiece.indexPiece - 9] >= 12
+      ) {
+        selectedPiece.negativeEighteenthSpace = true;
+      }
+    } else {
+      if (
+        board[selectedPiece.indexPiece + 14] === null &&
+        cells[selectedPiece.indexPiece + 14]?.classList.contains("noPieceHere") !== true &&
+        board[selectedPiece.indexPiece + 7] !== null &&
+        board[selectedPiece.indexPiece + 7] < 12 &&
+        board[selectedPiece.indexPiece + 7] !== null
+      ) {
+        selectedPiece.fourteenthSpace = true;
+      }
+      if (
+        board[selectedPiece.indexPiece + 18] === null &&
+        cells[selectedPiece.indexPiece + 18]?.classList.contains("noPieceHere") !== true &&
+        board[selectedPiece.indexPiece + 9] !== null &&
+        board[selectedPiece.indexPiece + 9] < 12 &&
+        board[selectedPiece.indexPiece + 9] !== null
+      ) {
+        selectedPiece.eighteenthSpace = true;
+      }
+      if (
+        board[selectedPiece.indexPiece - 14] === null &&
+        cells[selectedPiece.indexPiece - 14]?.classList.contains("noPieceHere") !== true &&
+        board[selectedPiece.indexPiece - 7] !== null &&
+        board[selectedPiece.indexPiece - 7] < 12 &&
+        board[selectedPiece.indexPiece - 7] !== null
+      ) {
+        selectedPiece.negativeFourteenthSpace = true;
+      }
+      if (
+        board[selectedPiece.indexPiece - 18] === null &&
+        cells[selectedPiece.indexPiece - 18]?.classList.contains("noPieceHere") !== true &&
+        board[selectedPiece.indexPiece - 9] !== null &&
+        board[selectedPiece.indexPiece - 9] < 12 &&
+        board[selectedPiece.indexPiece - 9] !== null
+      ) {
+        selectedPiece.negativeEighteenthSpace = true;
+      }
     }
     kingMovesRestrictions();
-}
+}  
+
+
 
  // erase restricts movement if the piece is a king so this piece can go back n forth
  
-function kingMovesRestrictions(){
+function kingMovesRestrictions():void {
     if (selectedPiece.isKing) {
         choosePieceBorder();
     } else {
@@ -256,10 +291,22 @@ function kingMovesRestrictions(){
 }
 // gives the choose piece a green color when clicks 
 
-function choosePieceBorder(){
-    if (selectedPiece.seventhSpace || selectedPiece.ninthSpace ||  selectedPiece.fourteenthSpace || selectedPiece.eightteenthSpace || selectedPiece.negativeSeventhSpace || selectedPiece.negativeNinthSpace ||  selectedPiece.negativefourteenthSpace || selectedPiece.negativeEighteenthSpace){
-        document.getElementById(selectedPiece.pieceId).style.border = "3px solid green";
-        onClickCells()
+function choosePieceBorder(): void {
+    if (
+        selectedPiece.seventhSpace ||
+        selectedPiece.ninthSpace ||
+        selectedPiece.fourteenthSpace ||
+        selectedPiece.eightteenthSpace ||
+        selectedPiece.negativeSeventhSpace ||
+        selectedPiece.negativeNinthSpace ||
+        selectedPiece.negativefourteenthSpace ||
+        selectedPiece.negativeEighteenthSpace
+    ) {
+        const selectedPieceElement = document.getElementById(String(selectedPiece.pieceId));
+        if (selectedPieceElement) {
+            selectedPieceElement.style.border = "3px solid green";
+            onClickCells();
+        }
     } else {
         return;
     }
@@ -271,7 +318,7 @@ function choosePieceBorder(){
 //  on clcik vs add event listener https://stackoverflow.com/questions/6348494/addeventlistener-vs-onclick
 //note : using anonymous function so we can remove it after , not a good idea use event listener cuz are unremovables 
 
-function onClickCells(){
+function onClickCells():void {
     if (selectedPiece.seventhSpace) {
         cells[selectedPiece.indexPiece + 7].setAttribute("onclick", "makeAMove(7)");
     }
@@ -301,47 +348,55 @@ function onClickCells(){
 //this funciton will take a numbr as an argument 
 // the number of cells the piece will move
 
-function makeAMove(number){
-    document.getElementById(selectedPiece.pieceId).remove(); 
-    cells[selectedPiece.indexPiece].innerHTML = "";
-    //get the id piece & remove it also change the cell to empty string  
-    if(turn){
-      if (selectedPiece.isKing) {
-          cells[selectedPiece.indexPiece + number].innerHTML = `<p class="red-piece king" id="${selectedPiece.pieceId}"></p>`;
-          redPieces = document.querySelectorAll("p"); 
-      }else{
-          cells[selectedPiece.indexPiece + number].innerHTML = `<p class="red-piece" id="${selectedPiece.pieceId}"></p>`;
-          redPieces = document.querySelectorAll("p");
+function makeAMove(number: number, removePiece: number): number {
+    if (removePiece) {
+      const selectedPieceElement = document.getElementById(String(selectedPiece.pieceId));
+      if (selectedPieceElement) {
+        selectedPieceElement.remove();
       }
-      //create a new element at the new cell that was clicked , then reset the red pieces variable so it can be save in the js memory , same with the black pieces  
-    }else{
-        if (selectedPiece.isKing) {
-            cells[selectedPiece.indexPiece + number].innerHTML = `<span class="black-piece king" id="${selectedPiece.pieceId}"></span>`;
-            blackPieces = document.querySelectorAll("span");
-        } else {
-            cells[selectedPiece.indexPiece + number].innerHTML = `<span class="black-piece" id="${selectedPiece.pieceId}"></span>`;
-            blackPieces = document.querySelectorAll("span");
-        }// this if statement is the same but with the black-pieces
     }
-    // this variable is made to pass the object propert directly into the function's argument
-
-    let boardPiece:number = selectedPiece.indexPiece 
-    if (number === 14 || number === -14 || number === 18 || number === -18) { // if the number of the move if a jump move 
-        changeTheData(boardPiece, boardPiece + number, boardPiece + number / 2);
-    }else{
-        changeTheData(boardPiece, boardPiece + number);
+  
+    cells[selectedPiece.indexPiece].innerHTML = "";
+    
+    if (turn) {
+      if (selectedPiece.isKing) {
+        cells[selectedPiece.indexPiece + number].innerHTML = `<p class="red-piece king" id="${selectedPiece.pieceId}"></p>`;
+        redPieces = document.querySelectorAll("p");
+      } else {
+        cells[selectedPiece.indexPiece + number].innerHTML = `<p class="red-piece" id="${selectedPiece.pieceId}"></p>`;
+        redPieces = document.querySelectorAll("p");
+      }
+    } else {
+      if (selectedPiece.isKing) {
+        cells[selectedPiece.indexPiece + number].innerHTML = `<span class="black-piece king" id="${selectedPiece.pieceId}"></span>`;
+        blackPieces = document.querySelectorAll("span");
+      } else {
+        cells[selectedPiece.indexPiece + number].innerHTML = `<span class="black-piece" id="${selectedPiece.pieceId}"></span>`;
+        blackPieces = document.querySelectorAll("span");
+      }
     }
-}
+  
+    let boardPiece: number = selectedPiece.indexPiece;
+  
+    if (number === 14 || number === -14 || number === 18 || number === -18) {
+      changeTheData(boardPiece, boardPiece + number, boardPiece + (number / 2));
+    } else {
+      changeTheData(boardPiece, boardPiece + number, removePiece);
+    }
+  
+    return selectedPiece.indexPiece + number;
+  }
+  
     //this function change the board data on the back end 
     // the parameter are the position , the new posititon & also the removepiece that got jumped
-function changeTheData(indexPiece, newIndex, removePiece){
+function changeTheData(indexPiece:number, newIndex:number, removePiece: number):void {
     board[indexPiece] = null;
     board[newIndex] = parseInt(selectedPiece.pieceId); //change the original position of the selected piece to null & move the data to the new position
     if(turn && selectedPiece.pieceId < 12 && newIndex >=57){  // 57 is the first cell of the last row . 12 is the last number of pieces 
-        document.getElementById(selectedPiece.pieceId).classList.add("king")
+        document.getElementById(selectedPiece.pieceId.toString())?.classList.add("king")
     }
     if(turn === false && selectedPiece.pieceId >= 12 && newIndex <= 7){ // 7 is the las cell in the first row this works for the black pieces 
-        document.getElementById(selectedPiece.pieceId).classList.add("king") 
+        document.getElementById(selectedPiece.pieceId.toString())?.classList.add("king") 
     }
     //if the position of the piece is >=51 (last row) then the iece become a king the same with the blacks using <+7
     // if a piece s removed change the data to null &  remove a point to the team score   
@@ -362,7 +417,7 @@ function changeTheData(indexPiece, newIndex, removePiece){
 }
 
  // remove an event listener to each piece , NOTE: in my plain english code , this was after the piecesEvent /lina 66 , but i though it was better so my code woouldnt be confused with the function remmove "Onclick" / ine 81
- function piecesEventRemove(){
+ function piecesEventRemove():void {
     if(turn){
         for (let i = 0; i < redPieces.length; i++) {
             redPieces[i].removeEventListener("click", getPiecescount);
@@ -376,7 +431,7 @@ function changeTheData(indexPiece, newIndex, removePiece){
 }
 // if the score  hit the 0 then show other team name as winner 
 // https://www.w3schools.com/jsref/met_win_alert.asp
-function winGame(){
+function winGame(): void{
     if (blackPieceScore === 0) {
         alert('BlacksWins');
     
@@ -390,7 +445,7 @@ function winGame(){
 
 //Switch players 
 // tthis function would show whos turn is, itiration of each letter changing the size & color
-function switchPlayer(){
+function switchPlayer(): void{
     if (turn) {
       turn = false;
       for (let i = 0; i < redTurnText.length ; i++) {
